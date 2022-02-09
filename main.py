a3b99eff27bb841a1b95c0dc4cf609b45c58b49f
Rezaul Karim Mamun <43470875+RKMAMUN@users.noreply.github.com>
2022-02-09 23:21:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from tkinter import *
 from tkinter import ttk
 import time
@@ -7,7 +8,9 @@ import os
 import sqlite3
 from tkinter import messagebox
 now = datetime.datetime.now()
+
 #----------- importing sqlite for server side operations---------------------------------------------------------------------------------
+
 con = sqlite3.Connection('hm_proj.db')
 cursor = con.cursor()
 cursor.execute("create table if not exists hoteld(t_r number,r_r number,t_s number)")
@@ -26,15 +29,15 @@ con.commit()
 con.commit()
 cursor.execute("select * from payments")
 con.commit()
-x=cursor.fetchall()
+x = cursor.fetchall()
 con.commit()
 
 #-----------splash_screen------------------------------------------------------------------------------------------------------------------
 sroot = Tk()
-sroot.minsize(height=516,width=1150)
+sroot.minsize(height=516, width=1150)
 sroot.configure(bg='skyblue')
 #chilanka
-Label(sroot,text="Hostel Management System ",font='Timesnewroman 40 ',bg='blue',fg='black').place(x=250,y=10)
+Label(sroot, text="Hostel Management System", font='Timesnewroman 40', bg='blue', fg='black').place(x=250, y=10)
 
 #Label(sroot,text="9589861196",font='Timesnewroman 40',bg='white',fg='grey').place(x=535,y=450)
 #----------- main project------------------------------------------------------------------------------------------------------------------
@@ -42,10 +45,11 @@ def mainroot():
 	#sroot.destroy()
 	root = Tk()
 	root.geometry('1080x500')
-	root.minsize(width=1080,height=550)
-	root.maxsize(width=1080,height=550)
+	root.minsize(width=1080, height=550)
+	root.maxsize(width=1080, height=550)
 	root.configure(bg='white')
 	root.title("Hostel Management System")
+
 	#--------------seperator-------------------------------------------------------------------------------------------------------------------
 
 	#sep = Frame(height=500,bd=1,relief='sunken',bg='white')
@@ -56,10 +60,10 @@ def mainroot():
 		os.startfile("C:/Users/TestFile.txt", "print")
 	#---------------top frame------------------------------------------------------------------------------------------------------------------
 
-	top_frame = Frame(root,height=70,width=1080,bg='skyblue')
+	top_frame = Frame(root, height=70, width=1080, bg='skyblue')
 
-	top_frame.place(x=0,y=0)
-	tf_label = Label(top_frame,text='Hostel Management System',font='msserif 33',fg='black',bg='skyblue',height=70)
+	top_frame.place(x=0, y=0)
+	tf_label = Label(top_frame, text='Hostel Management System', font='msserif 33', fg='black', bg='skyblue', height=70)
 	tf_label.pack(anchor='center')
 	top_frame.pack_propagate(False)
 
@@ -67,54 +71,56 @@ def mainroot():
 	def datetime():
 		#while(True):
 		localtime = now.strftime("%Y-%m-%d %H:%M")
-		lblInfo = Label(top_frame,font='helvetica 16',text=localtime,bg='blue',fg='white')
+		lblinfo = Label(top_frame, font='helvetica 16', text=localtime, bg='blue', fg='white')
+
 		#lblInfo.place(x=333,y=40)
 
 	#----------------bottom frame - hotel status and default page-------------------------------------------------------------------------------
 	def hotel_status():
 		global b_frame
-		b_frame = Frame(root,height=400,width=1080,bg='gray91')
-		b_frame.place(x=0,y=120+6+20+60+11)
+		b_frame = Frame(root,height=400, width=1080, bg='gray91')
+		b_frame.place(x=0, y=120+6+20+60+11)
 		b_frame.pack_propagate(False)
 		path = "images/newbg6lf.jpg"
 		img = ImageTk.PhotoImage(Image.open(path))
-		label = Label(b_frame,image = img ,height=400,width=1080)
+		label = Label(b_frame, image = img , height=400, width=1080)
 		label.image=img
-		label.place(x=0,y=0)
+		label.place(x=0, y=0)
 		cursor.execute("select * from hoteld")
 		x = cursor.fetchall()
 		#print(x)
 		cursor.execute("select count(rn) from roomd")
 		x = cursor.fetchone()
-		print (x)
+		print(x)
 		cursor.execute("select count(rn) from roomd where rstatus = 'Reserved'")
 		y = cursor.fetchone()
-		print (y)
+		print(y)
 		tor = x[0]
 		rer = y[0]
 		tos = 21
 		avr = int(tor)-int(rer)
 		avr = str(avr)
 		#print(tor,rer,tos,avr)
-		hts = Label(b_frame,text='Hotel Status',font='msserif 15',fg='black',bg='gray91',height=1)
+		hts = Label(b_frame, text='Hotel Status', font='msserif 15', fg='black', bg='gray91', height=1)
+
 	#------------inner frames of bottom frame-------------------------
 
-		smf1 = Frame(b_frame,height=150,width=175,bg='white')
-		tr = Label(smf1,text='Total Rooms:',fg='white',bg='cyan4',width=100,height=2,font='helvetica 15')
+		smf1 = Frame(b_frame, height=150, width=175, bg='white')
+		tr = Label(smf1, text='Total Rooms:', fg='white', bg='cyan4', width=100, height=2, font='helvetica 15')
 		tr.pack(side='top')
 		smf1.pack_propagate(False)
-		smf1.place(x=0,y=30)
-		Label(smf1,text=tor,fg='cyan4',bg='white',font='msserif 50').pack(anchor='center')
+		smf1.place(x=0, y=30)
+		Label(smf1, text=tor, fg='cyan4', bg='white', font='msserif 50').pack(anchor='center')
 
-		smf2 = Frame(b_frame,height=150,width=175,bg='white')
-		ar = Label(smf2,text='Available Rooms:',fg='white',bg='cyan4',width=130,height=2,font='helvetica 15')
+		smf2 = Frame(b_frame, height=150, width=175, bg='white')
+		ar = Label(smf2, text='Available Rooms:', fg='white', bg='cyan4', width=130, height=2, font='helvetica 15')
 		ar.pack(side='top')
 		smf2.pack_propagate(False)
-		smf2.place(x=180+4,y=30)
-		Label(smf2,text=avr,fg='cyan4',bg='white',font='msserif 50').pack(anchor='center')
+		smf2.place(x=180+4, y=30)
+		Label(smf2, text=avr, fg='cyan4', bg='white', font='msserif 50').pack(anchor='center')
 
-		smf3 = Frame(b_frame,height=150,width=175,bg='white')
-		tre = Label(smf3,text='Total reservations:',fg='white',bg='cyan4',width=130,height=2,font='helvetica 15')
+		smf3 = Frame(b_frame, height=150, width=175, bg='white')
+		tre = Label(smf3, text='Total reservations:', fg='white',bg='cyan4',width=130,height=2,font='helvetica 15')
 		tre.pack(side='top')
 		smf3.pack_propagate(False)
 		smf3.place(x=360+6,y=30)
@@ -131,38 +137,38 @@ def mainroot():
 		ts = Label(smf5,text='Total Staff:',fg='white',bg='cyan4',width=130,height=2,font='helvetica 15')
 		ts.pack(side='top')
 		smf5.pack_propagate(False)
-		smf5.place(x=720+10,y=30)
-		Label(smf5,text=tos,fg='cyan4',bg='white',font='msserif 50').pack(anchor='center')
-		redf1 = Frame(b_frame,height=8,width=1080,bg='cyan4')
+		smf5.place(x=720+10, y=30)
+		Label(smf5,text=tos, fg='cyan4', bg='white', font='msserif 50').pack(anchor='center')
+		redf1 = Frame(b_frame, height=8, width=1080, bg='cyan4')
 
-		smf6 = Frame(b_frame,height=150,width=175,bg='white')
-		ts = Label(smf6,text='Under renovation:',fg='white',bg='cyan4',width=130,height=2,font='helvetica 15')
+		smf6 = Frame(b_frame, height=150, width=175, bg='white')
+		ts = Label(smf6, text='Under renovation:', fg='white', bg='cyan4', width=130, height=2, font='helvetica 15')
 		ts.pack(side='top')
 		smf6.pack_propagate(False)
-		smf6.place(x=915,y=30)
-		Label(smf6,text='3',fg='cyan4',bg='white',font='msserif 50').place(x=60,y=60)
+		smf6.place(x=915, y=30)
+		Label(smf6, text='3', fg='cyan4', bg='white', font='msserif 50').place(x=60, y=60)
 		#redf1 = Frame(b_frame,height=8,width=1080,bg='cyan4')
 
 		#Label(b_frame,text='==================================================================================',fg='cyan4').place(x=0,y=20)
-		redf1.place(x=0,y=22)
-		Label(b_frame,text='Hotel Status',font='msserif 12',bg='cyan4',fg='white').pack(anchor='center')
+		redf1.place(x=0, y=22)
+		Label(b_frame, text='Hotel Status', font='msserif 12', bg='cyan4', fg='white').pack(anchor='center')
 		redf1.pack_propagate(False)
 		#b_frame.pack_propagate(False)
 
 	#-----------------------------------------------------------------
-		nl = Label(b_frame,text='www.itsourcecode.com',fg='blue',bg='gray91',font='msserif 8')
-		nl.place(x=955,y=310)
+		nl = Label(b_frame,text='www.itsourcecode.com', fg='blue', bg='gray91', font='msserif 8')
+		nl.place(x=955, y=310)
 		nl.tkraise()
 
 	#-------------- Guests --------------------------------------------------------------------------------------------------------------------------
 	def staff():
-		b_frame = Frame(root,height=400,width=1080,bg='white')
+		b_frame = Frame(root, height=400, width=1080, bg='white')
 		path = "images/newbg6lf.jpg"
 		img = ImageTk.PhotoImage(Image.open(path))
-		label = Label(b_frame,image = img ,height=400,width=1080)
-		label.image=img
-		label.place(x=0,y=0)
-		l = Label(b_frame,text='Details of Staff will be Available soon')
+		label = Label(b_frame, image = img , height=400, width=1080)
+		label.image = img
+		label.place(x=0, y=0)
+		l = Label(b_frame, text='Details of Staff will be Available soon')
 		#l.place(x=180,y=0)
 		'''smf4 = Frame(b_frame,height=150,width=175,bg='white')
 		tc = Label(smf4,text='Total Customers:',fg='white',bg='cyan4',width=130,height=2,font='helvetica 15')
@@ -174,7 +180,7 @@ def mainroot():
 		emp1f = Frame(b_frame)
 		path1 = "images/newman.jpg"
 		img1 = ImageTk.PhotoImage(Image.open(path1))
-		emp1 = Label(emp1f,image = img1)
+		emp1 = Label(emp1f, image = img1)
 		emp1.image=img1
 		emp1.pack()
 		emp1f.place(x=0,y=0)
@@ -231,41 +237,7 @@ def mainroot():
 		Label(emp1inf,text="Mail : charllote@gmail.com",bg='white',fg="Grey",font='msserif 10').place(x=55,y=83)
 		emp1inf.pack_propagate(False)
 		emp1inf.place(x=763,y=153)
-		#b_frame.pack_propagate(False)
-		'''sidebuttons = Text(b_frame,width=1,height=19)
-		sc = Scrollbar(b_frame,command=sidebuttons.yview,width=10,bg='lightsteelblue3')
-		sidebuttons.configure(yscrollcommand=sc.set)
-		sc.pack(side='left',fill=Y)
-		sidebuttons.place(x=10,y=0)
-		b1  = Button(b_frame,font='mssherif 10', text="Room 1", bg='white',fg='cyan4',width=10)
-		b2  = Button(b_frame,font='mssherif 10', text="Room 2", bg='white',fg='cyan4',width=10)
-		b3  = Button(b_frame,font='mssherif 10', text="Room 3", bg='white',fg='cyan4',width=10)
-		b4  = Button(b_frame,font='mssherif 10', text="Room 4", bg='white',fg='cyan4',width=10)
-		b5  = Button(b_frame,font='mssherif 10', text="Room 5", bg='white',fg='cyan4',width=10)
-		b6  = Button(b_frame,font='mssherif 10', text="Room 6", bg='white',fg='cyan4',width=10)
-		b7  = Button(b_frame,font='mssherif 10', text="Room 7", bg='white',fg='cyan4',width=10)
-		b8  = Button(b_frame,font='mssherif 10', text="Room 8", bg='white',fg='cyan4',width=10)
-		b9  = Button(b_frame,font='mssherif 10', text="Room 9", bg='white',fg='cyan4',width=10)
-		b10 = Button(b_frame,font='mssherif 10', text="Room 10",bg='white',fg='cyan4',width=10)
-		sidebuttons.window_create("end",window=b1)
-		sidebuttons.insert("end","\n")
-		sidebuttons.window_create("end",window=b2)
-		sidebuttons.insert("end","\n")
-		sidebuttons.window_create("end",window=b3)
-		sidebuttons.insert("end","\n")
-		sidebuttons.window_create("end",window=b4)
-		sidebuttons.insert("end","\n")
-		sidebuttons.window_create("end",window=b5)
-		sidebuttons.insert("end","\n")
-		sidebuttons.window_create("end",window=b6)
-		sidebuttons.insert("end","\n")
-		sidebuttons.window_create("end",window=b7)
-		sidebuttons.insert("end","\n")
-		sidebuttons.window_create("end",window=b8)
-		sidebuttons.insert("end","\n")
-		sidebuttons.window_create("end",window=b9)
-		sidebuttons.insert("end","\n")
-		sidebuttons.window_create("end",window=b10)'''
+
 		Frame(b_frame,height=13,width=250,bg='white').place(x=410,y=2)
 		Frame(b_frame,height=13,width=250,bg='white').place(x=410,y=153)
 		#Frame(b_frame,height=180,width=13,bg='white').place(x=406,y=20)
@@ -275,9 +247,7 @@ def mainroot():
 		b_frame.place(x=0,y=120+6+20+60+11)
 		b_frame.pack_propagate(False)
 		b_frame.tkraise()
-		nl = Label(b_frame,text='www.itsourcecode.com',fg='blue',bg='gray91',font='msserif 8')
-		nl.place(x=955,y=310)
-		nl.tkraise()
+
 
 	#-------------- rooms --------------------------------------------------------------------------------------------------------------------------
 	def rooms():
@@ -411,9 +381,7 @@ def mainroot():
 		sidebuttons.window_create("end",window=b19)
 		sidebuttons.insert("end","\n")
 		sidebuttons.window_create("end",window=b20)
-		#nl = Label(b_frame,text='www.itsourcecode.com',fg='blue',bg='gray91',font='msserif 8')
-		#nl.place(x=955,y=310)
-		#nl.tkraise()
+
 	#--------------- payments-----------------------------------------------------------------------------------------------------------------------
 
 	def payments():
@@ -531,18 +499,7 @@ def mainroot():
 			l1=Label(fl1,text='Transaction Date',bg='cyan4',fg='white',font='msserif 17')
 			idd = p_id.get()
 
-			'''fl5=Frame(b_frame,height=38,width=308,bg='cyan4')
-			fl5.place(x=0,y=150)
-			l1=Label(fl5,text='Paid for room',bg='cyan4',fg='white',font='msserif 17')
-			l1.pack()
-			fl5.pack_propagate(False)
 
-			fr5=Frame(b_frame,height=38,width=1080-308,bg='white')
-			fr5.place(x=0+308,y=150)
-			l1=Label(fl1,text='Date of transaction',bg='cyan4',fg='white',font='msserif 17')
-			#l1.pack()
-			fr5.pack_propagate(False)
-			'''
 
 
 	#cur.execute("create table if not exists paymentsf(id number  primary key,f_name varchar,l_name varchar,c_number varchar,email varchar , r_n number ,day varchar,month varchar,year varchar,time varchar , method varchar,totalamt varchar)")
@@ -558,15 +515,15 @@ def mainroot():
 				tot = yy[3]
 				ap = yy[4]
 				pm = '--'
-				l1=Label(fr1,text=dot,height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
-				l2=Label(fr2,text=tot,height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
-				l3=Label(fr3,text=ap,height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
-				l4=Label(fr4,text=pm,height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
+				l1 = Label(fr1,text=dot,height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
+				l2 = Label(fr2,text=tot,height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
+				l3 = Label(fr3,text=ap,height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
+				l4 = Label(fr4,text=pm,height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
 			else:
-				l1=Label(fr1,text='No Information Available',height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
-				l1=Label(fr2,text='No Information Available',height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
-				l1=Label(fr3,text='No Information Available',height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
-				l1=Label(fr4,text='No Information Available',height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
+				l1 = Label(fr1,text='No Information Available',height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
+				l1 = Label(fr2,text='No Information Available',height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
+				l1 = Label(fr3,text='No Information Available',height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
+				l1 = Label(fr4,text='No Information Available',height=38,width=1080-308,font='msserif 15',bg='white',fg='cyan4').pack()
 		ok = Button(hline,text='OK',font='msserif 10',bg='white',activebackground='steelblue',fg='cyan4',command=getid)
 		ok.place(x=530,y=5)
 		p_id.bind('<Return>',getid)
@@ -575,47 +532,11 @@ def mainroot():
 		pinv = Button(b_frame,text='Print reciept',bg='Green',fg='white',command=pr).place(x=976,y=235)
 		#------------inner frames of bottom frame-------------------------
 
-		'''smf1 = Frame(b_frame,height=150,width=175,bg='white')
-		tr = Label(smf1,text='Total Rooms:',fg='white',bg='cyan4',width=100,height=2,font='helvetica 15')
-		tr.pack(side='top')
-		smf1.pack_propagate(False)
-		smf1.place(x=0,y=30)
-		Label(smf1,fg='cyan4',bg='white',font='msserif 50').pack(anchor='center')
 
-		smf2 = Frame(b_frame,height=150,width=175,bg='white')
-		ar = Label(smf2,text='Available Rooms:',fg='white',bg='cyan4',width=130,height=2,font='helvetica 15')
-		ar.pack(side='top')
-		smf2.pack_propagate(False)
-		smf2.place(x=180+4,y=30)
-		Label(smf2,fg='cyan4',bg='white',font='msserif 50').place(x=63,y=60)
 
-		smf3 = Frame(b_frame,height=150,width=175,bg='white')
-		tre = Label(smf3,text='Total reservations:',fg='white',bg='cyan4',width=130,height=2,font='helvetica 15')
-		tre.pack(side='top')
-		smf3.pack_propagate(False)
-		smf3.place(x=360+6,y=30)
-		Label(smf3,fg='cyan4',bg='white',font='msserif 50').pack(anchor='center')
-
-		smf4 = Frame(b_frame,height=150,width=175,bg='white')
-		tc = Label(smf4,text='Total Customers:',fg='white',bg='cyan4',width=130,height=2,font='helvetica 15')
-		tc.pack(side='top')
-		smf4.pack_propagate(False)
-		smf4.place(x=540+8,y=30)
-		Label(smf4,text='40',fg='cyan4',bg='white',font='msserif 50').pack(anchor='center')
-
-		smf5 = Frame(b_frame,height=150,width=175,bg='white')
-		ts = Label(smf5,text='Total Staff:',fg='white',bg='cyan4',width=130,height=2,font='helvetica 15')
-		ts.pack(side='top')
-		smf5.pack_propagate(False)
-		smf5.place(x=720+10,y=30)
-		Label(smf5,fg='cyan4',bg='white',font='msserif 50').pack(anchor='center')
-		#Label(b_frame,text='==================================================================================',fg='cyan4').place(x=0,y=20)
-		#b_frame.pack_propagate(False)
-	'''
-
-		nl = Label(b_frame,text='www.itsourcecode.com',fg='blue',bg='gray91',font='msserif 8')
-		nl.place(x=955,y=310)
-		nl.tkraise()
+		# nl = Label(b_frame,text='www.itsourcecode.com',fg='blue',bg='gray91',font='msserif 8')
+		# nl.place(x=955,y=310)
+		# nl.tkraise()
 
 	#---------------reserve------------------------------------------------------------------------------------------------------------------------
 
@@ -875,13 +796,13 @@ def mainroot():
 		wifib = IntVar()
 
 		style = ttk.Style()
-		style.map('TCombobox', fieldbackground=[('readonly','white')])
-		Label(b_frame,text='Bed(s) :',bg='gray93',font='17').place(x=730,y=50)
+		style.map('TCombobox', fieldbackground=[('readonly', 'white')])
+		Label(b_frame, text='Bed(s) :', bg='gray93', font='17').place(x=730, y=50)
 		#Radiobutton(b_frame,text='1',bg='gray93',variable=nbb,value=1,font='15',width=3).place(x=800,y=50)
 		#Radiobutton(b_frame,text='2',bg='gray93',variable=nbb,value=2,font='15',width=3).place(x=880,y=50)
 		#Radiobutton(b_frame,text='3',bg='gray93',variable=nbb,value=3,font='15',width=3).place(x=960,y=50)
 		nb = ttk.Combobox(b_frame,values=['please select...','1','2','3'],state='readonly',width=22)
-		nb.place(x=830,y=50)
+		nb.place(x=830, y=50)
 		nb.current(0)
 
 		Label(b_frame,text='AC :',font='17',bg='gray93').place(x=732,y=75)
